11176d08d403a8e80e75acf8f65c73a2bdad5073
mancinilucas <mancini.lucasg@gmail.com>
2022-03-01 15:15:14 -0300

    refactor componentes

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@chakra-ui/react";
+export function Logo(){
+  return(
+    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64" >
+      dashgo
+      <Text as="span" ml="1" color="blue.500" >.</Text>
+    </Text>
+  )
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import dynamic from 'next/dynamic';
-import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
+import { Header } from "../components/Header/Index";
+import { Sidebar } from "../components/SideBar";
 import { ApexOptions } from 'apexcharts';
 
 const Chart = dynamic(()=> import('react-apexcharts'),{
